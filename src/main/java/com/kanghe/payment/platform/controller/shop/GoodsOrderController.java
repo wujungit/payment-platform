91bfa9882d3f98ac1c2e56bc9828daaa8424011b
wujun <18813918091@163.com>
2019-05-28 17:44:56 +0800

    添加演示商城

--- src/main/java/com/kanghe/payment/platform/controller/shop/GoodsOrderController.java
+++ src/main/java/com/kanghe/payment/platform/controller/shop/GoodsOrderController.java
@@ -1,11 +1,19 @@
 package com.kanghe.payment.platform.controller.shop;
 
+import com.kanghe.payment.platform.entity.GoodsOrder;
 import com.kanghe.payment.platform.service.GoodsOrderService;
+import com.kanghe.payment.platform.util.AmountUtil;
+import com.kanghe.payment.platform.util.DateUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author: W_jun1
@@ -23,6 +31,98 @@ public class GoodsOrderController {
     @RequestMapping("/openQrPay.html")
     public String openQrPay(ModelMap model) {
         return "openQrPay";
+    }
+
+    @RequestMapping("/qrPay.html")
+    public String qrPay(ModelMap model, HttpServletRequest request, Long amount) {
+        String logPrefix = "【二维码扫码支付】";
+        String view = "qrPay";
+        log.info("====== 开始接收二维码扫码支付请求 ======");
+        String ua = request.getHeader("User-Agent");
+        String goodsId = "G_0001";
+        log.info("{}接收参数:goodsId={},amount={},ua={}", logPrefix, goodsId, amount, ua);
+        String client = "alipay";
+        String channelId = "ALIPAY_WAP";
+        if (StringUtils.isBlank(ua)) {
+            String errorMessage = "User-Agent为空！";
+            log.info("{}信息：{}", logPrefix, errorMessage);
+            model.put("result", "failed");
+            model.put("resMsg", errorMessage);
+            return view;
+        } else {
+            if (ua.contains("Alipay")) {
+                client = "alipay";
+                channelId = "ALIPAY_WAP";
+            } else if (ua.contains("MicroMessenger")) {
+                client = "wx";
+                channelId = "WX_JSAPI";
+            }
+        }
+        if (client == null) {
+            String errorMessage = "请用微信或支付宝扫码";
+            log.info("{}信息：{}", logPrefix, errorMessage);
+            model.put("result", "failed");
+            model.put("resMsg", errorMessage);
+            return view;
+        }
+        // 先插入订单数据
+        GoodsOrder goodsOrder = null;
+        Map<String, String> orderMap = null;
+        if ("alipay".equals(client)) {
+            log.info("{}{}扫码下单", logPrefix, "支付宝");
+            Map params = new HashMap<>();
+            params.put("channelId", channelId);
+            // 下单
+            goodsOrder = createGoodsOrder(goodsId, amount);
+            orderMap = createPayOrder(goodsOrder, params);
+        } else if ("wx".equals(client)) {
+            log.info("{}{}扫码", logPrefix, "微信");
+            // 判断是否拿到openid，如果没有则去获取
+            String openId = request.getParameter("openId");
+            if (StringUtils.isNotBlank(openId)) {
+                log.info("{}openId：{}", logPrefix, openId);
+                Map params = new HashMap<>();
+                params.put("channelId", channelId);
+                params.put("openId", openId);
+                goodsOrder = createGoodsOrder(goodsId, amount);
+                // 下单
+                orderMap = createPayOrder(goodsOrder, params);
+            } else {
+                String redirectUrl = QR_PAY_URL + "?amount=" + amount;
+                String url = GetOpenIdURL2 + "?redirectUrl=" + redirectUrl;
+                log.info("跳转URL={}", url);
+                return "redirect:" + url;
+            }
+        }
+        model.put("goodsOrder", goodsOrder);
+        model.put("amount", AmountUtil.convertCent2Dollar(goodsOrder.getAmount() + ""));
+        if (orderMap != null) {
+            model.put("orderMap", orderMap);
+            String payOrderId = orderMap.get("payOrderId");
+            GoodsOrder go = new GoodsOrder();
+            go.setGoodsOrderId(goodsOrder.getGoodsOrderId());
+            go.setPayOrderId(payOrderId);
+            go.setChannelId(channelId);
+            int ret = goodsOrderService.update(go);
+            log.info("修改商品订单,返回:{}", ret);
+        }
+        model.put("client", client);
+        return view;
+    }
+
+    GoodsOrder createGoodsOrder(String goodsId, Long amount) {
+        // 先插入订单数据
+        String goodsOrderId = String.format("%s%s%06d", "G", DateUtil.getSeqString(), (int) seq.getAndIncrement() % 1000000);
+        GoodsOrder goodsOrder = new GoodsOrder();
+        goodsOrder.setGoodsOrderId(goodsOrderId);
+        goodsOrder.setGoodsId(goodsId);
+        goodsOrder.setGoodsName("XXPAY捐助商品G_0001");
+        goodsOrder.setAmount(amount);
+        goodsOrder.setUserId("xxpay_000001");
+        goodsOrder.setStatus(Constant.GOODS_ORDER_STATUS_INIT);
+        int result = goodsOrderService.addGoodsOrder(goodsOrder);
+        log.info("插入商品订单,返回:{}", result);
+        return goodsOrder;
     }
 
 }
